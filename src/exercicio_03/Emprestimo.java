fe1710043643917e3b88eb70681bf1e526a5f1f1
Loveneykens Philogene <loveneykens@gmail.com>
2021-12-01 19:32:16 -0300

    exercicio_03 atualizado!

--- src/exercicio_03/Emprestimo.java
+++ src/exercicio_03/Emprestimo.java
@@ -1,44 +1,71 @@
 package exercicio_03;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Emprestimo {
 
     public static void main(String[] args) {
+
+        final int numeroMaximoDeParcelas = 12;
         double valorDoEmprestimo = 0d;
         int numeroDasParcelas = 0;
-        double valorDeCadaParcela = 0d;
-        final int numeroMaximoDeParcelas = 12;
-        double valorTotalDasParcelas = 0d;
-        int indiceDasParcelas = 1;
-
+        BigDecimal valorDeCadaParcela = new BigDecimal(0d);
+        BigDecimal valorTotalDasParcelas = new BigDecimal(0d);
+        BigDecimal somaDasParcelas = new BigDecimal(0d);
 
         DecimalFormat dFormatter = new DecimalFormat("#.00");
         Scanner s = new Scanner(System.in);
+
         try (s) {
             System.out.println("Digita o valor do emprestimo : ");
             valorDoEmprestimo = s.nextDouble();
             System.out.println("Digita o numero de parcelas: ");
             numeroDasParcelas = s.nextInt();
-            if (numeroDasParcelas <= numeroMaximoDeParcelas) {
-                System.out.println("-------------------- \n" + "Parcelas \n" + "--------------------");
-                for (indiceDasParcelas = 1; indiceDasParcelas <= numeroDasParcelas; indiceDasParcelas++) {
 
-                    for (int indiceDeCadaValor = 0; indiceDeCadaValor < 1; indiceDeCadaValor++) {
-
-                        valorDeCadaParcela = valorDoEmprestimo / numeroDasParcelas;
-                        valorTotalDasParcelas += valorDeCadaParcela;
-
-                    }
-                    System.out.println("Parcela 0" + indiceDasParcelas + " :" + dFormatter.format(valorDeCadaParcela));
-                }
-
-                System.out.println("-------------------- \n" + "Total .............: R$" + dFormatter.format(valorTotalDasParcelas));
-
+            if ((valorDoEmprestimo <= 0) && numeroDasParcelas <= 0) {
+                System.out.println("Quantidade de parcelas e o valor de emprestimo não podem ser 0.");
 
             } else {
-                System.out.println("O numero maximo de parcelas é 12.");
+
+                if (numeroDasParcelas <= numeroMaximoDeParcelas) {
+                    System.out.println("-------------------- \n" + "Parcelas \n" + "--------------------");
+
+                    valorDeCadaParcela = new BigDecimal(valorDoEmprestimo).divide(BigDecimal.valueOf(numeroDasParcelas), 2, RoundingMode.HALF_DOWN);
+
+                    for (int indiceDasParcelas = 1; indiceDasParcelas <= numeroDasParcelas; indiceDasParcelas++) {
+
+                        somaDasParcelas = somaDasParcelas.add(valorDeCadaParcela);
+
+                        if ((indiceDasParcelas == numeroDasParcelas) && (somaDasParcelas.doubleValue() < valorDoEmprestimo)) {
+                            BigDecimal restoDoValorDoEmprestimo = new BigDecimal(valorDoEmprestimo).subtract(somaDasParcelas);
+                            valorDeCadaParcela = valorDeCadaParcela.add(restoDoValorDoEmprestimo);
+
+                        } else {
+                            valorDeCadaParcela = valorDeCadaParcela;
+
+                        }
+
+                        valorTotalDasParcelas = valorTotalDasParcelas.add(valorDeCadaParcela);
+                        System.out.println("Parcela 0" + indiceDasParcelas + ": " + "R$" + valorDeCadaParcela);
+
+                    }
+
+                    if (valorDoEmprestimo < valorTotalDasParcelas.doubleValue()) {
+                        BigDecimal valorAMaisDsomaDasParcelas = valorTotalDasParcelas.subtract(new BigDecimal(valorDoEmprestimo));
+                        valorTotalDasParcelas = valorTotalDasParcelas.subtract(valorAMaisDsomaDasParcelas);
+
+                    } else {
+                        valorTotalDasParcelas = valorTotalDasParcelas;
+
+                    }
+
+                    System.out.println("-------------------- \n" + "Total .............: R$" + valorTotalDasParcelas);
+                } else {
+                    System.out.println("O numero maximo de parcelas é 12.");
+                }
             }
 
         } catch (Exception e) {
