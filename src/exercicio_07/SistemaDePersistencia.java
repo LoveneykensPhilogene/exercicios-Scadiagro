b710aace3a6642943fc4c394f8c8599f836e76db
Loveneykens Philogene <loveneykens@gmail.com>
2021-12-13 20:47:34 -0300

    classe SistemaDePersistencia, responsável para manipular todas as regras de negócios.

--- src/exercicio_07/SistemaDePersistencia.java
+++ src/exercicio_07/SistemaDePersistencia.java
@@ -6,25 +6,60 @@ import exercicio_06.ListaEncadeada;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class SistemaDePersistencia {
 
+    private static PersistenciaDeDados persistence;
+
+    private static ListaEncadeada listaDosFuncionarios;
+    private static Funcionario funcionario;
+    private static Funcionario funcionario1;
+    private static Funcionario funcionario2;
+    private static int chaveDeEntrada;
+
     public static void main(String[] args) {
-        PersistenciaDeDados persistence = new PersistenciaDeDados();
+        persistence = new PersistenciaDeDados();
 
-        ListaEncadeada listaDosFuncionarios = new ListaEncadeada();
-        Funcionario funcionario = new Funcionario(12, "Jose", new BigDecimal(1500.00), LocalDate.parse("2021/06/10", DateTimeFormatter.ofPattern("yyyy/MM/dd")));
-        Funcionario funcionario1 = new Funcionario(1, "Carlos", new BigDecimal(1d), LocalDate.parse("2020/08/20", DateTimeFormatter.ofPattern("yyyy/MM/dd")));
-        Funcionario funcionario2 = new Funcionario(13, "Pierre", new BigDecimal(200.56), LocalDate.parse("2019/09/30", DateTimeFormatter.ofPattern("yyyy/MM/dd")));
+        listaDosFuncionarios = new ListaEncadeada();
+        funcionario = new Funcionario(12, "Jose", new BigDecimal(1500.00), LocalDate.parse("2021/06/10", DateTimeFormatter.ofPattern("yyyy/MM/dd")));
+        funcionario1 = new Funcionario(1, "Carlos", new BigDecimal(1d), LocalDate.parse("2020/08/20", DateTimeFormatter.ofPattern("yyyy/MM/dd")));
+        funcionario2 = new Funcionario(13, "Pierre", new BigDecimal(200.56), LocalDate.parse("2019/09/30", DateTimeFormatter.ofPattern("yyyy/MM/dd")));
 
-        listaDosFuncionarios.adicionarPrimeiroElemento(funcionario);
-        listaDosFuncionarios.adicionarPrimeiroElemento(funcionario1);
-        listaDosFuncionarios.adicionarPrimeiroElemento(funcionario2);
+        listaDosFuncionarios.adicionarElementoNoInicio(funcionario);
+        listaDosFuncionarios.adicionarElementoNoInicio(funcionario1);
+        listaDosFuncionarios.adicionarElementoNoInicio(funcionario2);
+
         persistence.setDados(listaDosFuncionarios);
-        persistence.gravarDadosDosFuncionarios(persistence.getDados());
 
-        persistence.lerDadosDosFuncionarios();
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Logado com sucessso!");
+        System.out.println("Se você quer adicionar dados nos arquivos , aperta 1 : \n");
+        System.out.println("Se você quer adicionar dados nos arquivos , aperta 2 : ");
+        try (entrada) {
+            chaveDeEntrada=Integer.parseInt(entrada.nextLine());
+            if (chaveDeEntrada == 1) {
+                persistence.gravarDadosDosFuncionarios(persistence.getDados());
+                persistence.gravarDadosOrdenadosPorCodigo(persistence.getDados());
+                persistence.gravarDadosOrdenadosPorNome(persistence.getDados());
 
+                if(persistence.getDados()!=null){
+                    System.out.println("Dados gravados com sucesso!");
+                }else {
+                    System.out.println(new Exception("Lista vazia"));
+                }
+            }
 
+            if (chaveDeEntrada == 2) {
+                persistence.lerDadosDosFuncionarios();
+
+                persistence.lerDadosIndexadosPorCodigo();
+
+                persistence.lerDadosIndexadosPorNome();
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
