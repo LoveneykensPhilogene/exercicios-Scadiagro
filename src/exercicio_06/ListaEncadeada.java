2eb7cd5dd7d388d2b132d97489a1171fed93143c
Loveneykens Philogene <loveneykens@gmail.com>
2021-12-13 22:40:29 -0300

    regra de negócios

--- src/exercicio_06/ListaEncadeada.java
+++ src/exercicio_06/ListaEncadeada.java
@@ -2,6 +2,11 @@ package exercicio_06;
 
 import exercicio_05.Funcionario;
 
+import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class ListaEncadeada {
     private Elemento inicio;
     private Elemento fim;
@@ -31,42 +36,125 @@ public class ListaEncadeada {
         this.tamanho = tamanho;
     }
 
-    public void adicionarPrimeiroElemento(Funcionario funcionario) {
+    public boolean listaVazia() {
+        boolean empty = getInicio() == null;
+        return empty;
+    }
 
+    public void tamanhoDaLista() {
+        setTamanho(0);
+        while (getInicio() != null) {
+            tamanho++;
+            setInicio(getInicio().getProximaPosicao());
+        }
+        System.out.println(getTamanho());
+    }
+
+    public void adicionarElementoNoInicio(Funcionario funcionario) {
+
+        funcionario = dadosDoFuncionarioFormatado(funcionario);
         Elemento elemento = new Elemento();
-        this.tamanho = 0;
         elemento.setFuncionario(funcionario);
-        elemento.setPosicaoAnterior(null);
-        elemento.setProximaPosicao(this.inicio);
-
-        if (this.inicio != null) {
-            this.inicio.setPosicaoAnterior(elemento);
-        }
-        this.inicio = elemento;
-
-        if (this.tamanho.equals(0)) {
-            this.fim = this.inicio;
+        elemento.setProximaPosicao(getInicio());
+        setInicio(elemento);
+        if (getInicio() == null) {
+            setInicio(elemento);
+            setFim(getInicio());
+            setInicio(getFim());
+        } else {
+            setInicio(elemento);
         }
 
     }
 
-    public void ordenarPeloCodigoDoElemento(ListaEncadeada listaDosElemento) {
+    public void adicionarElementoNoFim(Funcionario funcionario) {
+        Elemento elemento = new Elemento(funcionario);
+        if (getInicio().equals(null)) {
+            setInicio(elemento);
+            setFim(elemento);
+        } else {
+            getFim().setPosicaoAnterior(elemento);
+            setFim(elemento);
+        }
+    }
 
+    public void ordenarPeloCodigoDoElemento() {
+
+        List<Funcionario> novoElemento = new ArrayList<Funcionario>();
+        Elemento elemento = getInicio();
+
+        while (elemento != null) {
+
+            novoElemento.add(elemento.getFuncionario());
+            novoElemento.sort(Comparator.comparingInt(Funcionario::getCod_funcionario));
+            elemento = elemento.getProximaPosicao();
+        }
+        System.out.println("\n====Ordenada por código===\n");
+        for (int indiceDoNovoElemento = 0; indiceDoNovoElemento < novoElemento.size(); indiceDoNovoElemento++) {
+            ListaEncadeada lista = new ListaEncadeada();
+            lista.adicionarElementoNoInicio(novoElemento.get(indiceDoNovoElemento));
+            System.out.println(lista.getInicio().getFuncionario());
+        }
 
     }
 
-    public void ordenarPeloNomeDoElemento() {
+    public void ordenarPeloNomeElemento() {
 
+        List<Funcionario> novoElemento = new ArrayList<Funcionario>();
+        Elemento elemento = getInicio();
+
+        while (elemento != null) {
+
+            novoElemento.add(elemento.getFuncionario());
+            novoElemento.sort(Comparator.comparing(Funcionario::getNome));
+            elemento = elemento.getProximaPosicao();
+        }
+        System.out.println("\n====Ordenada por nome===\n");
+        for (int indiceDoNovoElemento = 0; indiceDoNovoElemento < novoElemento.size(); indiceDoNovoElemento++) {
+            ListaEncadeada lista = new ListaEncadeada();
+            lista.adicionarElementoNoInicio(novoElemento.get(indiceDoNovoElemento));
+            System.out.println(lista.getInicio().getFuncionario());
+        }
 
     }
-
 
     public void listaDosElementos() {
         Elemento elemento = this.inicio;
         while (elemento != null) {
-
             System.out.println(elemento.getFuncionario());
             elemento = elemento.getProximaPosicao();
+        }
+
+    }
+
+    public Funcionario dadosDoFuncionarioFormatado(Funcionario funcionario) {
+        Funcionario funcionarioFormatado = new Funcionario();
+
+        funcionarioFormatado.setCod_funcionario(funcionario.getCod_funcionario());
+        funcionarioFormatado.setNome(funcionario.getNome());
+        funcionarioFormatado.setValorSalario(funcionario.getValorSalario().setScale(2, RoundingMode.DOWN));
+        funcionarioFormatado.setDataAdmissao(funcionario.getDataAdmissao());
+
+        return funcionarioFormatado;
+    }
+
+    public void instrucao() {
+
+        System.out.println("========================\n" + "*Instrução*\n"
+                + "Opção 1 =>Digite número 1 : "
+                + "Para iniciar o programa \n"
+                + "Opção 2 =>Digite número 0 :"
+                + "Encerrar a sessão."
+                + "\n========================\n");
+    }
+
+    public void abrirSistema(int entrada) {
+
+        int btnLigar = 1;
+        if (btnLigar == entrada) {
+            System.out.println("Você está logado.");
+        } else {
+            System.out.println(new Exception("Digitou um número errado") + "\nSegue a intrução");
         }
 
     }
