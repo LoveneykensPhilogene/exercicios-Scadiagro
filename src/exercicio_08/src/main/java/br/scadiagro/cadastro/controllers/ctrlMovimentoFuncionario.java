9a1b3dcba49cddc53a749e8da5014051bc57cae8
Loveneykens Philogene <loveneykens@gmail.com>
2021-12-24 01:48:23 -0300

    Controlar a tela visão geral e também tem relação entre todas as classes do pacote controllers

--- src/exercicio_08/src/main/java/br/scadiagro/cadastro/controllers/ctrlMovimentoFuncionario.java
+++ src/exercicio_08/src/main/java/br/scadiagro/cadastro/controllers/ctrlMovimentoFuncionario.java
@@ -1,7 +1,5 @@
 package br.scadiagro.cadastro.controllers;
 
-import br.scadiagro.cadastro.CadastroApplication;
-import br.scadiagro.cadastro.ControleApplication;
 import br.scadiagro.cadastro.model.Funcionario;
 import br.scadiagro.cadastro.util.lFuncionarioRepository;
 import javafx.beans.property.SimpleObjectProperty;
@@ -10,24 +8,22 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
+import javafx.scene.layout.AnchorPane;
 
 import java.io.IOException;
 import java.math.BigDecimal;
-import java.net.URL;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ResourceBundle;
 
-public class ctrlMovimentoFuncionario implements Initializable {
+import static javafx.application.Platform.exit;
+
+public class ctrlMovimentoFuncionario extends AnchorPane {
 
     @FXML
-    private List<Funcionario> todosFuncionario = new ArrayList<>();
+    private List<Funcionario> todosFuncionarios = new ArrayList<>();
 
     @FXML
     public TableView<Funcionario> tbRelatorio = new TableView<>();
@@ -44,51 +40,74 @@ public class ctrlMovimentoFuncionario implements Initializable {
     @FXML
     public TableColumn<Funcionario, LocalDate> tbColunaData = new TableColumn<>();
 
-    public ObservableList<Funcionario> observableList;
-
     @FXML
     public Button btnSair;
 
-    public ctrlMovimentoFuncionario() {
-    }
+    @FXML
+    private AnchorPane root;
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        try {
-            DefinirTabela();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    @FXML
+    private MenuButton mnbConsulta;
 
+    @FXML
+    private MenuItem mItemCodigo;
+
+    @FXML
+    private MenuItem mItemNome;
+
+    @FXML
+    private Button tlbNovoCadastro;
+
+    @FXML
+    private Button tlbRelatorio;
+
+    @FXML
+    private Button tbnSair;
+
+    public TextField teste;
+
+    private Funcionario oFunProcurado;
+
+    public ObservableList<Funcionario> observableList;
+
+    public ctrlMovimentoFuncionario() throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/br/scadiagro/cadastro/view/dsListaFuncionario.fxml"));
+        fxmlLoader.setRoot(this);
+        fxmlLoader.setController(this);
+        fxmlLoader.load();
+        DefinirTabela();
     }
 
     @FXML
     public void BtnSair(ActionEvent event) throws IOException {
-        btnSair.getScene().getWindow();
+        exit();
     }
 
     @FXML
     public void BtnNovo(ActionEvent event) throws Exception {
-        ctrlCadasFuncionario cadasFuncionario = new ctrlCadasFuncionario();
-        CadastroApplication appCadastro = new CadastroApplication();
-        appCadastro.start(new Stage());
-        appCadastro.stage.show();
+        ctrlCadasFuncionario cadFuncionario = new ctrlCadasFuncionario(this.getScene());
+        cadFuncionario.abrir();
+        DefinirTabela();
+    }
 
+    @FXML
+    public void MostrarTelaDeControle(ActionEvent event) throws IOException {
+        ctrlControle oCtrlControle = new ctrlControle(this.getScene());
+        oCtrlControle.Abrir();
     }
 
     @FXML
     public void DefinirTabela() throws Exception {
         lFuncionarioRepository oRepository = new lFuncionarioRepository();
-        todosFuncionario = oRepository.BuscarTodosOsFuncionarios("", oRepository.getsArquivoFuncionario());
+        todosFuncionarios = oRepository.BuscarTodosOsFuncionarios("", oRepository.getsArquivoFuncionario());
 
         tbColunaCodigo.setCellValueFactory(cod -> new SimpleObjectProperty<>(cod.getValue().getnCodFuncionario()));
         tbColunaNome.setCellValueFactory(nome -> new SimpleStringProperty(nome.getValue().getsNome()));
         tbColunaSalario.setCellValueFactory(sal -> new SimpleObjectProperty<>(sal.getValue().getnSalario()));
         tbColunaData.setCellValueFactory(dat -> new SimpleObjectProperty<>(dat.getValue().getdData()));
-        observableList = FXCollections.observableArrayList(todosFuncionario);
+        observableList = FXCollections.observableArrayList(todosFuncionarios);
         tbRelatorio.setItems(observableList);
         tbRelatorio.setEditable(true);
-
 
     }
 
@@ -100,15 +119,9 @@ public class ctrlMovimentoFuncionario implements Initializable {
         tbRelatorio.refresh();
 
         listCodigo = oRepository.BuscarTodosOsFuncionarios("", oRepository.getsArquivoOrdenadoPorCodigo());
-
-        tbColunaCodigo.setCellValueFactory(cod -> new SimpleObjectProperty<>(cod.getValue().getnCodFuncionario()));
-        tbColunaNome.setCellValueFactory(nome -> new SimpleStringProperty(nome.getValue().getsNome()));
-        tbColunaSalario.setCellValueFactory(sal -> new SimpleObjectProperty<>(sal.getValue().getnSalario()));
-        tbColunaData.setCellValueFactory(dat -> new SimpleObjectProperty<>(dat.getValue().getdData()));
         observableList = FXCollections.observableArrayList(listCodigo);
         tbRelatorio.setItems(observableList);
         tbRelatorio.setEditable(true);
-
 
     }
 
@@ -120,15 +133,9 @@ public class ctrlMovimentoFuncionario implements Initializable {
         tbRelatorio.refresh();
 
         listPorNome = oRepository.BuscarTodosOsFuncionarios("", oRepository.getsArquivoOrdenadoPorNome());
-
-        tbColunaCodigo.setCellValueFactory(cod -> new SimpleObjectProperty<>(cod.getValue().getnCodFuncionario()));
-        tbColunaNome.setCellValueFactory(nome -> new SimpleStringProperty(nome.getValue().getsNome()));
-        tbColunaSalario.setCellValueFactory(sal -> new SimpleObjectProperty<>(sal.getValue().getnSalario()));
-        tbColunaData.setCellValueFactory(dat -> new SimpleObjectProperty<>(dat.getValue().getdData()));
         observableList = FXCollections.observableArrayList(listPorNome);
         tbRelatorio.setItems(observableList);
         tbRelatorio.setEditable(true);
-
 
     }
 
@@ -136,12 +143,7 @@ public class ctrlMovimentoFuncionario implements Initializable {
     public void MenorSalario(ActionEvent actionEvent) {
         lFuncionarioRepository oRepository = new lFuncionarioRepository();
         Funcionario oFuncionario = oRepository.BuscarFuncionarioCOmMenorSalario(oRepository.getsArquivoFuncionario());
-        todosFuncionario.clear();
 
-        tbColunaCodigo.setCellValueFactory(cod -> new SimpleObjectProperty<>(cod.getValue().getnCodFuncionario()));
-        tbColunaNome.setCellValueFactory(nome -> new SimpleStringProperty(nome.getValue().getsNome()));
-        tbColunaSalario.setCellValueFactory(sal -> new SimpleObjectProperty<>(sal.getValue().getnSalario()));
-        tbColunaData.setCellValueFactory(dat -> new SimpleObjectProperty<>(dat.getValue().getdData()));
         observableList = FXCollections.observableArrayList(oFuncionario);
         tbRelatorio.setItems(observableList);
         tbRelatorio.setEditable(true);
@@ -151,29 +153,73 @@ public class ctrlMovimentoFuncionario implements Initializable {
     public void MaiorSalario(ActionEvent actionEvent) {
         lFuncionarioRepository oRepository = new lFuncionarioRepository();
         Funcionario oFuncionario = oRepository.BuscarFuncionarioCOmMaiorSalario(oRepository.getsArquivoFuncionario());
-        todosFuncionario.clear();
 
-        tbColunaCodigo.setCellValueFactory(cod -> new SimpleObjectProperty<>(cod.getValue().getnCodFuncionario()));
-        tbColunaNome.setCellValueFactory(nome -> new SimpleStringProperty(nome.getValue().getsNome()));
-        tbColunaSalario.setCellValueFactory(sal -> new SimpleObjectProperty<>(sal.getValue().getnSalario()));
-        tbColunaData.setCellValueFactory(dat -> new SimpleObjectProperty<>(dat.getValue().getdData()));
         observableList = FXCollections.observableArrayList(oFuncionario);
         tbRelatorio.setItems(observableList);
         tbRelatorio.setEditable(true);
     }
 
     @FXML
-    public void MostrarTelaDeControle(ActionEvent event) throws IOException {
-        ControleApplication oAppControle = new ControleApplication();
-        oAppControle.start(new Stage());
-        oAppControle.stage.show();
+    public void MenuAtualizarFuncionario(ActionEvent actionEvent) throws Exception {
+        ctrlAtualizar oCtrlAtualizar = new ctrlAtualizar(this.getScene());
+        oCtrlAtualizar.getStage().setTitle("Atualizar funcionário");
+        lFuncionarioRepository oRepository = new lFuncionarioRepository();
+        oFunProcurado = new Funcionario();
+        oFunProcurado = tbRelatorio.getSelectionModel().getSelectedItem();
+        oRepository.CriarArquivo("", "db//update//selectFuncionario.dat", oFunProcurado);
+        oCtrlAtualizar.Abrir();
+
+    }
+
+    public void BtnAtualizarFuncionario(String sNome, BigDecimal nSalario) throws Exception {
+        lFuncionarioRepository oRepository = new lFuncionarioRepository();
+        oFunProcurado = oRepository.BuscarFuncionarioCOmMenorSalario("db//update//selectFuncionario.dat");
+        Funcionario oFunAtulizado = oRepository.AtualizarFuncionario(oFunProcurado, new Funcionario(oFunProcurado.getnCodFuncionario(), sNome.toUpperCase()
+                , nSalario, oFunProcurado.getdData()));
+        observableList = FXCollections.observableArrayList(oFunAtulizado);
+        tbRelatorio.setItems(observableList);
+        tbRelatorio.setEditable(true);
+        tbRelatorio.refresh();
+        DefinirTabela();
+    }
+
+    @FXML
+    public void MenuBuscarFuncionario(ActionEvent actionEvent) throws IOException {
+        ctrlBuscarFuncionario oCtrlBuscar = new ctrlBuscarFuncionario(this.getScene());
+        oCtrlBuscar.Abrir();
+    }
+
+    public void BuscarFunPeloCodigo(Long nCodigo) throws Exception {
+        lFuncionarioRepository oRepository = new lFuncionarioRepository();
+        Funcionario oFunComCodigo = oRepository.BuscarUmFuncionarioPorCodigo(nCodigo);
+        observableList = FXCollections.observableArrayList(oFunComCodigo);
+        tbRelatorio.setItems(observableList);
+        tbRelatorio.setEditable(true);
+        tbRelatorio.refresh();
+        DefinirTabela();
     }
 
     @FXML
     public void BtnEcluirTodos(ActionEvent actionEvent) throws Exception {
-
+        lFuncionarioRepository oRepository = new lFuncionarioRepository();
+        oRepository.ExcTodosFuncionarios(oRepository.getsArquivoFuncionario());
+        DefinirTabela();
     }
 
+    @FXML
+    public void BtnExcFuncionario() throws Exception {
+        lFuncionarioRepository oRepository = new lFuncionarioRepository();
+        if (tbRelatorio.getSelectionModel().getSelectedItem() != null) {
+            Funcionario oFunc = tbRelatorio.getSelectionModel().getSelectedItem();
+            oRepository.ExcluirFuncionario(oFunc);
+            tbRelatorio.setEditable(true);
+            tbRelatorio.refresh();
+            DefinirTabela();
+        } else {
+            Alert oAlert = new Alert(Alert.AlertType.CONFIRMATION, "ERRO!");
+            oAlert.setHeaderText("Deve selecionar o funcionário que deseja excluir");
+        }
+    }
 
 }
 
